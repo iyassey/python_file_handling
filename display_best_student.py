a6ef0d09cd3c56f8cbb26ff5b82b6c43a8e4bbfc
Thea <uythea487102@gmail.com>
2024-04-21 11:55:45 +0800

    Pseudocode

---
+++ display_best_student.py
@@ -0,0 +1,41 @@
+#author__thea_uy
+#date_april_21_2024
+
+#this program will read a file containing the name of the 20 students together with their GWA
+#this program will also display the name and GWA of the student/s who got the highest GWA
+
+
+#import modules for display purposes
+
+
+#list variables
+
+
+#read text file classlist.txt 
+
+
+#read every line of the text file classlist.txt
+
+
+#extract the full name of the students
+
+
+#extract the grades of the students
+
+
+#determine the highest grade among the 20 students
+
+
+#join the the lists containing the full name of the students and the grade of the students
+
+
+#display message for the students
+
+
+#determine which student/s got the highest grade
+
+
+#display the sstudent/s who got the highest grade
+
+
+#this is the end of the program
