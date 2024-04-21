ccff4c1902605220851aede95e1230841b5cc660
Thea <uythea487102@gmail.com>
2024-04-21 14:41:47 +0800

    Pseudocode

---
+++ power_integers.py
@@ -0,0 +1,35 @@
+#author_thea_uy
+#april_21_2024
+
+#this program will create two separate text files after reading the source text file named integers.txt that contains 20 integers
+#the first output file will be named double.txt containing the square of all even integers found in integers.txt
+#the second file will be named triple.txt containing the sube of all odd numbers found in the integers.txt
+
+#import modules
+
+
+#list variables
+
+
+#read the text file integers.txt
+
+
+#read every line of the text file integers.txt and convert from string to integers
+
+
+#determine if the numbers are even or odd
+
+
+#get the square value of the even numbers from integers.txt
+
+
+#get the cube value of the odd numbers from integers.txt
+
+
+#create a new text file for square numbers
+
+
+#create a new text file for cube numbers
+
+
+#this is the end of the program
