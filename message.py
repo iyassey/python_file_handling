848ff70a67e31ae7d6bf08dbf79ba0b743b791d1
Thea <uythea487102@gmail.com>
2024-04-21 13:46:51 +0800

    Pseudocode

---
+++ message.py
@@ -0,0 +1,25 @@
+#author__thea_uy
+#date_april_21_2024
+
+#this program will write multiple line of text contents into a text file mylife.txt
+
+#initialize a loop
+
+
+#variable for the input
+
+
+#ask the user for the number of lines
+
+
+#let the user enter their message
+
+
+#create a text file mylife.txt
+
+
+#ask the user if they want to try again
+
+
+
+#this is the end of the program!
