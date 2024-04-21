c8f254f22ee32dd25baf281347242d2df279b01f
Thea <uythea487102@gmail.com>
2024-04-21 08:33:19 +0800

    Pseudocode

---
+++ read_integers.py
@@ -0,0 +1,27 @@
+#author__thea_uy
+#date_april_21_2024
+
+#this program will read text file nnamed numbers.txt taht contains 20 integers
+#this program will also create two text file;
+#thhis first text file will be named even.txt that will contain all even numbers extracted from numbers.txt
+#the second text file will be named odd.txt that will contain all odd numbers extracted from numbers.txt
+
+#list variables
+
+
+#read the text file numbers.txt
+
+
+#read every line of the text file numbers.txt & convert string to integer
+
+
+#determine if the numbers are even or odd
+
+
+#create a new text file for even numbers
+
+
+#create a new text file for odd numbers
+
+
+#this is the end of the program
